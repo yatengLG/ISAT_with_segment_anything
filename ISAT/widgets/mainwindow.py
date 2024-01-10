a9edfa1dfa63740ab1f77f0c987d191ec33e9a9f
Hongyuan Zhang <66273343+Alias-z@users.noreply.github.com>
2024-01-10 15:45:03 +0100

    Update mainwindow.py

--- ISAT/widgets/mainwindow.py
+++ ISAT/widgets/mainwindow.py
@@ -468,15 +468,13 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.image_root = dir
         self.actionOpen_dir.setStatusTip("Image root: {}".format(self.image_root))
 
+        self.label_root = dir
+        self.actionSave_dir.setStatusTip("Label root: {}".format(self.label_root))
+
         if os.path.exists(os.path.join(dir, 'isat.yaml')):
-            self.label_root = dir
-            self.actionSave_dir.setStatusTip("Label root: {}".format(self.label_root))
             # load setting yaml
-            if os.path.exists(os.path.join(dir, 'isat.yaml')):
-                self.config_file = os.path.join(dir, 'isat.yaml')
-                self.reload_cfg()
-        else:
-            self.label_root = None
+            self.config_file = os.path.join(dir, 'isat.yaml')
+            self.reload_cfg()
 
         self.show_image(self.current_index)
 

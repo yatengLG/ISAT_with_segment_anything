a2e4cf7b2f0071a016a4351f230490e2f507474f
yatenglg <767624851@qq.com>
2025-08-06 15:47:59 +0800

    - docs

---
+++ docs/source/category_setting.rst
@@ -0,0 +1,40 @@
+Category Setting
+=================================
+
+1. Category Setting
+---------------------------------
+
+In ISAT, you can customize category names and their display colors.
+
+Click the ``Category settings`` button to open the category settings interface.
+
+.. image:: ../../display/category_setting.png
+    :alt: category_setting.png
+
+.. tip:: The background category is not modifiable in ISAT.
+
+2. Select Category
+---------------------------------
+
+Before starting the annotation, you need to select a category.
+
+.. image:: ../../display/category_dock.png
+    :alt: category_dock.png
+
+If the targets to be annotated belong to the same category,  it can save a lot of work.
+
+2.1 Category Selection Shortcut
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+Numbers ``[1 - 9]`` are category selection shortcut keys, allowing you to quickly select the category.
+
+2.2 Quickly Locate the Category
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+ISAT uses a fuzzy matching algorithm to locate the desired category from a large number of categories.
+
+.. image:: ../../display/category_search.gif
+    :alt: category_search.gif
+
+.. tip:: The shortcuts from 1 to 9 also apply to the filtered categories.
+
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,6 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-
 project = 'ISAT-SAM'
 copyright = '2025, Shuwei Ji and Hongyuan Zhang'
 author = 'Shuwei Ji and Hongyuan Zhang'
@@ -14,30 +13,19 @@ author = 'Shuwei Ji and Hongyuan Zhang'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+extensions = ['sphinx.ext.duration']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'EN'
+language = 'en'
+
 locale_dirs = ['locales/']
 gettext_compact = False
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
 

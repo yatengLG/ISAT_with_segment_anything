0cb0c6f1169b4e1934865f692360b2402a9665ad
yatenglg <767624851@qq.com>
2025-12-17 10:43:55 +0800

    为text prompt添加多类别支持。

--- ISAT/widgets/text_prompt_dock_widget.py
+++ ISAT/widgets/text_prompt_dock_widget.py
@@ -14,10 +14,25 @@ class TextPromptDockWidget(QtWidgets.QWidget, Ui_Form):
         self.setupUi(self)
         self.mainwindow = mainwindow
 
-        self.pushButton_predict.clicked.connect(self.predict)
+        self.pushButton_predict_for_single_category.clicked.connect(self.predict_for_single_category)
+        self.pushButton_catgories_setting.clicked.connect(self.mainwindow.category_setting)
+        self.pushButton_predict_for_multi_categoriies.clicked.connect(self.predict_for_multi_categories)
 
-    def predict(self):
+    def predict_for_single_category(self):
         self.label_num_objects.setText("......")
         self.label_num_objects.repaint()
-        num_masks = self.mainwindow.predict_current_image_with_text_prompt(self.lineEdit_prompt_text.text())
+        num_masks = self.mainwindow.predict_current_image_with_text_prompt(
+            self.lineEdit_prompt_text_for_single_category.text()
+        )
         self.label_num_objects.setText(f"{num_masks}")
+
+    def predict_for_multi_categories(self):
+        self.label_num_objects.setText("......")
+        self.label_num_objects.repaint()
+        num_masks_all = 0
+        for category, _ in self.mainwindow.category_color_dict.items():
+            if category == "__background__":
+                continue
+            num_masks = self.mainwindow.predict_current_image_with_text_prompt(category)
+            num_masks_all += num_masks
+        self.label_num_objects.setText(f"{num_masks_all}")
